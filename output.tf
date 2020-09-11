87b21e45b827a4453d52d2718d9be6816c0affa7
Slav Donchev <slav.donchev@gmail.com>
2020-09-11 10:19:13 +0300

    add output descriptions

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "instance_private_ip" {
-  description = ""
+  description = "The private IP of the EC2 instance."
   value       = aws_instance.gitlab.private_ip
 }
 
 output "instance_public_ip" {
-  description = ""
+  description = "The public IP of the EC2 instance."
   value       = try(data.aws_eip.external_eip[0].public_ip, aws_instance.gitlab.public_ip)
 }
 
 output "instance_public_dns" {
-  description = ""
+  description = "The public DNS of the EC2 instance."
   value       = try(data.aws_eip.external_eip[0].public_dns, aws_instance.gitlab.public_dns)
 }
 
 output "gitlab_url" {
-  description = ""
-  value       = coalesce(var.gitlab_url, "http://${try(data.aws_eip.external_eip[0].public_dns, aws_instance.gitlab.public_dns)}")
+  description = "The URL of the GitLab instance."
+  value       = coalesce(var.gitlab_url, "http://${try(data.aws_eip.external_eip[0].public_dns, aws_instance.gitlab.public_dns, aws_instance.gitlab.private_dns)}")
 }
